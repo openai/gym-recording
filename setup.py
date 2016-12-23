2a979d4832d0c7d2b7b327a5e7862126ed5f4d37
Richard Möhn <richard.moehn@posteo.de>
2016-12-23 15:30:33 +0900

    setup.py: Add packages
    Pip wasn't installing any code, because there was no 'packages' entry in
    setup.py. Add it.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_recording',
       version='0.0.1',
       install_requires=['gym', 'boto3'],
+      packages=find_packages(),
 )
