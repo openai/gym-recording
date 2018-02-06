046b832b8873e42190c9258fd5ab49b7f166c1ef
Daniel Angelov <dani_angelov@yahoo.com>
2018-02-06 17:59:56 +0000

    Make plot prettier.

--- test/play.py
+++ test/play.py
@@ -5,7 +5,8 @@ import numpy as np
 def handle_ep(observations, actions, rewards, infos):
 
     plt.ion()
-    fig = plt.figure(); 
+    fig = plt.figure()
+    fig.gca().set_aspect('equal', adjustable='box')
     ax = fig.gca() 
     xs = np.array([]) 
     ys = np.array([]) 
@@ -19,13 +20,14 @@ def handle_ep(observations, actions, rewards, infos):
     print('\n\n\n\nAn episode begins!')
     for obs, a, r, i in zip(observations, actions, rewards, infos):
         print(': {} {} {} {}'.format(obs, a, r, i))
-        x = i['self_state']['lat']
-        y = i['self_state']['lon']
+        x = i['self_state']['lon']
+        y = i['self_state']['lat']
         xs = np.append(xs,x); 
         ys = np.append(ys,y); 
-    plt.plot(xs, ys, 'r')
+    plt.plot(xs, ys, '-o')
 
 
 if __name__ == '__main__':
-   playback.scan_recorded_traces('./t', handle_ep)
+   # playback.scan_recorded_traces('./t', handle_ep)
+    playback.scan_recorded_traces('/home/daniel/code/cogle/ddpg-craft/scripts/traces', handle_ep)
     plt.show()
