2f4e1852c42295467c6c904e6fced5feb8dedea9
Daniel Angelov <dani_angelov@yahoo.com>
2018-03-14 14:39:10 +0000

    Updated plotting method.

--- test/play.py
+++ test/play.py
@@ -1,6 +1,7 @@
 from gym_recording import playback
 import matplotlib.pyplot as plt
 import numpy as np
+import glob, os
 
 def handle_ep(observations, actions, rewards, infos):
 
@@ -8,26 +9,34 @@ def handle_ep(observations, actions, rewards, infos):
     fig = plt.figure()
     fig.gca().set_aspect('equal', adjustable='box')
     ax = fig.gca() 
+
     xs = np.array([]) 
-    ys = np.array([]) 
+    ys = np.array([])
+    alts = np.array([]) 
 
     # plot empty line to generate line object 
-    line, = ax.plot(xs,ys) 
-
+    line, = ax.plot(xs, ys) 
 
     plt.ioff() # turn off interactive mode 
 
     print('\n\n\n\nAn episode begins!')
     for obs, a, r, i in zip(observations, actions, rewards, infos):
-        print(': {} {} {} {}'.format(obs, a, r, i))
-        x = i['self_state']['lon']
-        y = i['self_state']['lat']
-        xs = np.append(xs,x); 
-        ys = np.append(ys,y); 
-    plt.plot(xs, ys, '-o')
+        print('Obs: {} a: {} r: {} info: {}'.format(obs, a, r, i))
+        if i:
+            x = i['self_state']['lon']
+            y = i['self_state']['lat']
+            alt = i['relative_alt']
+            xs = np.append(xs,x)
+            ys = np.append(ys,y)
+            alts = np.append(alts, alt)
+
+    plt.plot(xs, ys, 'o-')
 
 
 if __name__ == '__main__':
-   # playback.scan_recorded_traces('./t', handle_ep)
-    playback.scan_recorded_traces('/home/daniel/code/cogle/ddpg-craft/scripts/traces', handle_ep)
+    path = '/tmp/gym/traces/'
+    files = glob.glob(os.path.join(path, '*'))
+    files.sort()
+    
+    playback.scan_recorded_traces(files[-1], handle_ep)
     plt.show()
